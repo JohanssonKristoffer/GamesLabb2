f8c3ca234697d90de7871b07eb9f8be103ca7f63
XyrWL <peter.trullenque@gmail.com>
2016-04-27 16:31:21 +0200

    TicTacToe started.

---
+++ KKGGames-Labb2/Controllers/TicTacToeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KKGGames_Labb2.Models;
+
+namespace KKGGames_Labb2.Controllers
+{
+    public class TicTacToeController : Controller
+    {
+        // GET: TicTacToe
+        public ActionResult Index()
+        {
+            return View(new TicTacToeModel());
+        }
+
+        // POST: TicTacToe
+        [HttpPost]
+        public ActionResult Index(TicTacToeModel model, string chosenCell)
+        {
+            if (Session["CellBoard"] != null)
+                model.CellBoard = (Cell[][])Session["CellBoard"];
+
+            model.ParseChosenCell(chosenCell);
+            if (model.TryPlaceChosenCell())
+                model.ComputerTurn();
+
+            Session["CellBoard"] = model.CellBoard;
+            return View(model);
+        }
+    }
+}
---
+++ KKGGames-Labb2/Models/TicTacToeModels.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KKGGames_Labb2.Models
+{
+    public class TicTacToeModel
+    {
+        const int XMAX = 3, YMAX = XMAX;
+
+        public Cell[][] CellBoard { get; set; }
+        public Coordinate ChosenCell { get; set; }
+
+        public TicTacToeModel()
+        {
+            CellBoard = new Cell[XMAX][];
+            for (int i = 0; i < XMAX; i++)
+                CellBoard[i] = new Cell[YMAX];
+        }
+
+        public void ParseChosenCell(string chosenCell)
+        {
+            ChosenCell = new Coordinate
+            {
+                X = int.Parse(chosenCell.Split(',')[0]),
+                Y = int.Parse(chosenCell.Split(',')[1])
+            };
+        }
+
+        public bool TryPlaceChosenCell()
+        {
+            if (CellBoard[ChosenCell.X][ChosenCell.Y] == Cell.Empty)
+            {
+                CellBoard[ChosenCell.X][ChosenCell.Y] = Cell.Player;
+                return true;
+            }
+            return false;
+        }
+
+        public void ComputerTurn()
+        {
+            //TBI
+        }
+    }
+
+    public class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public enum Cell
+    {
+        Empty,
+        Player,
+        Computer
+    }
+}
